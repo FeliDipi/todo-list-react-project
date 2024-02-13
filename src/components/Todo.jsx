1bbca7bbc4d1e1cd616769b717b89392d8ce3bfb
NicoDipi <nicolas.dipierro@7r1ck.com>
2024-02-12 23:31:10 -0300

    *filter bug fixed, and edit todo optimized

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,22 +1,32 @@
+import { useState } from "react";
 import "../styles/Todo.css";
 
 const Todo = ({onEdit, onDelete, id, title, description, date, isCompleted}) =>
 {
+    const [info, setInfo] = useState({title,description});
+
     const checkClass = isCompleted ? "todo-item-content is-check" : "todo-item-content";
 
     const handleCheck = () =>
     {
-        onEdit({"id":id, "title":title, "description":description, "isCompleted":!isCompleted});
+        onEdit({"id":id, "title":info.title, "description":info.description, "isCompleted":!isCompleted});
     }
 
     const handleChangeTitle = (event) =>
     {
-        onEdit({"id":id, "title":event.target.value, "description":description, "isCompleted":!isCompleted});
+        const newInfo = {"title":event.target.value,"description":info.description};
+        setInfo(newInfo);
     }
 
     const handleChangeDescription = (event) =>
     {
-        onEdit({"id":id, "title":title, "description":event.target.value, "isCompleted":!isCompleted});
+        const newInfo = {"title":info.title,"description":event.target.value};
+        setInfo(newInfo);
+    }
+
+    const handleFinishChanges = () =>
+    {
+        onEdit({"id":id, "title":info.title, "description":info.description, "isCompleted":isCompleted});
     }
 
     const handleDeleteTodo = () =>
@@ -27,8 +37,8 @@ const Todo = ({onEdit, onDelete, id, title, description, date, isCompleted}) =>
     return (
         <li className={checkClass}>
             <div className="todo-item-info-content">
-                <input onChange={handleChangeTitle} type="text" className="todo-item-info-title" value={title}/>
-                <input onChange={handleChangeDescription} type="text" className="todo-item-info-description" value={description}/>
+                <input onChange={handleChangeTitle} onBlur={handleFinishChanges} type="text" className="todo-item-info-title" value={info.title}/>
+                <input onChange={handleChangeDescription} onBlur={handleFinishChanges} type="text" className="todo-item-info-description" value={info.description}/>
             </div>
             <p className="todo-item-date">{date}</p>
             <div className="todo-item-inputs">
